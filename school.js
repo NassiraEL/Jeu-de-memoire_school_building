8b80a527becc3a855fba6341fccf3d1b7ecaf8bd
Nassira EL <nassirans01@gmail.com>
2023-11-05 13:22:37 +0100

    Ajout d'une partie js au jeu de mémoire

--- school.js
+++ school.js
@@ -1,11 +1,39 @@
-const winBefore = document.querySelector('.win::before');
 const wins = document.querySelectorAll('.win');
+let image1 = null;
+let image2 = null;
+let imgsrc1 = null;
+let imgsrc2 = null;
 
-wins.forEach(win=>{
+wins.forEach(win => {
   win.addEventListener('click', () => {
-    win.classList.toggle('height');
-    win.classList.add('rideau');
-})
+    if (image1 === null) {
+      win.classList.toggle('height');
+      win.classList.add('rideau');
 
+      setTimeout(() => {
+        win.classList.remove('height');
+      }, 1000);
 
+      image1 = win;
+      imgsrc1 = image1.querySelector('img').src;
+    
+    } else {
+      win.classList.toggle('height');
+      win.classList.add('rideau');
+
+      setTimeout(() => {
+        win.classList.remove('height');
+      }, 1000);
+
+        image2 = win;
+        imgsrc2 = image2.querySelector('img').src;
+        if (imgsrc1 !== imgsrc2) {
+          image1.classList.remove('rideau');
+          image2.classList.remove('rideau');
+        }
+
+      image1 = null;
+      image2 = null;
+    }
+  });
 });
